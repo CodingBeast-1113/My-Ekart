e09b102910f1ef06a2ff567798bfac8b97cc16be
CodingBeast-1113 <127874682+CodingBeast-1113@users.noreply.github.com>
2023-07-09 15:04:13 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,9 +19,9 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <Header/>
         {children}
-        
+        <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"/>
         </body>
-    <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"/>
+    
     </html>
   )
 }
